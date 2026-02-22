d05e4f94fab10fcbf3ae5644f01642f1577c2451
hgkimdev <hgkimdev@proton.me>
2026-02-22 23:43:19 +0900

    02-22

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+
+const styles = StyleSheet.create({
+  safearea: {
+    flex: 1,
+  },
+});
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={styles.safearea}>
+      <WebView
+        source={{ uri: "https://m.naver.com" }}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        onShouldStartLoadWithRequest={(request) => {
+          console.log("Home - request", request);
+
+          if (
+            request.url.startsWith("http://m.naver.com") ||
+            request.mainDocumentURL?.startsWith("http://m.naver.com")
+          ) {
+            return true;
+          }
+
+          if (request.url != null && request.url.startsWith("https://")) {
+            router.navigate({
+              pathname: "browser",
+            });
+            return false;
+          }
+          return true;
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
---
+++ app/(tabs)/shopping.tsx
@@ -0,0 +1,24 @@
+import { router } from "expo-router";
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
+const ShoppingScreen = () => {
+  return (
+    <View>
+      <Text>Shopping</Text>
+      <TouchableOpacity
+        onPress={() => {
+          router.navigate({
+            pathname: "browser",
+          });
+        }}
+      >
+        <Text>Go to Browser</Text>
+      </TouchableOpacity>
+      <MaterialCommunityIcons name="shopping" />
+    </View>
+  );
+};
+
+export default ShoppingScreen;
